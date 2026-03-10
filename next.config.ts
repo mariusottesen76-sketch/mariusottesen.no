6bb9231b2414b33e13ab94da36bd91ebb7ebd01f
Marius Ottesen <DIN_GITHUB_EMAIL>
2026-03-10 15:16:56 +0100

    Change sales-coach to redirect

--- next.config.ts
+++ next.config.ts
@@ -7,9 +7,14 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       { source: "/favicon.ico", destination: "/images/moc.logo.png" },
+    ];
+  },
+  async redirects() {
+    return [
       {
         source: "/sales-coach",
         destination: "https://predictive-sales-coach-v2.vercel.app",
+        permanent: false,
       },
     ];
   },
